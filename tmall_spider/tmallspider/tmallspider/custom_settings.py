c66ae885b7255749d5e974875dbe810103e936b4
liangchengzhu <liangchengzhu@91waijiao.com>
2014-03-11 20:21:11 +0800

    add project config file

---
+++ tmall_spider/tmallspider/tmallspider/custom_settings.py
@@ -0,0 +1,18 @@
+#-*- coding: UTF-8 -*- 
+
+u'''
+@author: jack_lcz
+@date: 2014-1-10
+'''
+
+# --------------------------------------
+# 自定义配置
+FILEPIPELINE_GOODSFILE_PATH = './goods.txt'
+FILEPIPELINE_SHOPSFILE_PATH = './shops.txt'
+FILEPIPELINE_SHOPDETAILSFILE_PATH = './shopdetails.txt'
+
+
+SingleMONGODB_SERVER = 'localhost'
+SingleMONGODB_PORT = 27017
+SingleMONGODB_DB = 'tmallspider'
+# --------------------------------------
